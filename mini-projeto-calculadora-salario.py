fac5232681ed084b7a85f49f3a8531c205994c85
Marcelo Silva dos Santos <marcelosilva3620@gmail.com>
2025-10-17 19:39:36 -0300

    Create mini-projeto-calculadora-salario.py
    Adiciona script inicial para cálculo de aumento salarial em Python.

---
+++ mini-projeto-calculadora-salario.py
@@ -0,0 +1,23 @@
+print('Olá, querido funcionário! Seja bem- vindo!')
+n1 = input('Por favor, informe o seu nome completo:')
+print(
+    f'Tudo preparado(a) {n1}?\n Vamos calcular o aumento do seu salário em 15%')
+
+# RECEBE O VALOR COMO TEXTO (COM VÍRGULAS E PONTOS, NO PADRÃO BR)
+valor_digitado = (input('Digite o valor do seu salário atualmente:'))
+
+# USA O REPLACE PARA REMOVER O SEPARADOR DE MILHAR (PONTO)
+valorlimpo = valor_digitado.replace('.', '')
+valorfinal = valorlimpo.replace('.', '')
+salario = float(valorfinal)
+if salario < 2000:
+    print('Nossa... talvez seja uma boa mudar de emprego!')
+
+elif salario == 2000:
+    print('Seu salário é de R$2000.00. Um valor OK, mas com aumento de 15 % vai melhorar!')
+
+else:
+    print('Uau! Belo salário!')
+
+conta = salario*1.15
+print(f'{n1} o seu salário com aumento de 15%, ficou no valor de R${conta:.2f}')
